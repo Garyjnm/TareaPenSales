e6d981c361352ab264da907613b8c7606df34c2e
Garyjnm <garyn7674@gmail.com>
2025-04-13 16:56:30 -0600

    Avances del segundo ejercicio

--- CostosDeEnvio.py
+++ CostosDeEnvio.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 file_path = "./Data/Pen Sales Data.xlsx"
 df_pen_sales = pd.read_excel(file_path, sheet_name="Pen Sales")
 
-# print(df_pen_sales.dtypes)
 
 df_avg_pen_costs = df_pen_sales.groupby("Item")["Shipping Cost"].mean().sort_values(ascending=True)
 print(df_avg_pen_costs)
--- PopularidadDeBoligrafos.py
+++ PopularidadDeBoligrafos.py
@@ -1,3 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+file_path = "./Data/Pen Sales Data.xlsx"
+df_pen_sales = pd.read_excel(file_path, sheet_name="Pen Sales")
+
+conteo_de_productos = df_pen_sales["Item"].value_counts()
+plt.figure(figsize = (10, 5))
+conteo_de_productos.plot(kind="barh", color = "green")
+plt.title("Ranking de popularidad de Productos")
+plt.xlabel("Cantidad de ventas")
+plt.ylabel("Tipo de productos")
+plt.gca().invert_yaxis()
+plt.show()
